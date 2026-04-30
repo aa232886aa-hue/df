2a01e89453b2a0cd60d69e78ae53b8845b30cf95
aa232886aa-hue <aa232886aa@gmail.com>
2026-04-30 22:13:16 +0300

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,9 @@ const nextConfig = {
   output: 'export',
   images: { unoptimized: true },
   eslint: { ignoreDuringBuilds: true },
-  typescript: { ignoreBuildErrors: true }
+  typescript: { ignoreBuildErrors: true },
+  // זה ימנע קריסה בזמן יצירת הדפים הסטטיים
+  staticPageGenerationTimeout: 1000,
 };
 
 module.exports = nextConfig;
